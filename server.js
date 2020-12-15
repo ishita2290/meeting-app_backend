5fe08564acb6fe9376a293d4d9eaf91129e8c932
ishita <ishitag077@gmail.com>
2020-12-15 11:24:23 +0100

    updated

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
-const dotenv = require('dotenv');
-const mongoose = require('mongoose');
-const express = require('express');
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const express = require("express");
+const User = require('./Models/User');
 
-const cors = require('cors');
+const cors = require("cors");
 const app = express();
 
 app.use(express.json());
@@ -10,17 +11,17 @@ app.use(cors());
 dotenv.config();
 
 const DB = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/${process.env.DB_NAME}?retryWrites=true&w=majority`;
-mongoose.connect(DB, {
+mongoose
+  .connect(DB, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-    .then ( () => {
-        console.log('DB connection successfull')
-    })
-    .catch ( error => {
-        console.log(`Cannot connect to database, error: ${error}`)
-    });
-app.get('/', (request, response) => {
-})
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("DB connection successfull");
+  })
+  .catch((error) => {
+    console.log(`Cannot connect to database, error: ${error}`);
+  });
+app.get("/", (request, response) => {});
 
-app.listen(4014, console.log('Server started'));
+app.listen(4014, console.log("Server started"));
