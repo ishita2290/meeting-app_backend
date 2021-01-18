5360019e84c97ef54cb46ff95e94043ac6a09313
Marzena Studniarz <marzena.kupiec@gmail.com>
2021-01-18 15:07:49 +0100

    [get category endpoint]

--- routes/events.js
+++ routes/events.js
@@ -3,26 +3,6 @@ const { builtinModules } = require('module');
 const mongoose = require('mongoose');
 const Event = require('../Models/EventModel');
 
-
-// router.post('/add-new-event', (request, response) => {
-
-//     Event.create(request.body).then(() =>  {
-//         console.log('it worked')
-//         response.status(201).json(
-//             {
-//                 status: 'New Event added',
-//                 data: {
-//                     event: Event //???                                                  
-//                 }
-//             })
-//     })                                                          
-//       .catch(error => response.status(400).json({
-//         status: 'fail',
-//         message: error
-//     }));
-       
-// });
-
 router.post('/add-new-event', async (request, response) => {
 
     try {
@@ -40,5 +20,45 @@ router.post('/add-new-event', async (request, response) => {
     }
        
 });
+
+
+// get data based on event's name
+
+router.get('/search-events/:query', async (request, response) => {
+
+    const {query} = request.params;
+
+    try {
+        const nameWhichContains = query;
+        const regex = new RegExp(nameWhichContains, 'i');
+        const events = await Event.find({eventName: {$regex: regex}});
+        response.json(events);
+    }
+    catch (error) {
+        console.error(error);
+    }
+
+});
+
+
+// get data based on category
+
+router.get('/search-events/', async (request, response) => {
+
+       const category = request.query.category
+
+    try {
+        const events = await Event.find({category });
+        response.json(events);
+    }
+    catch (error) {
+        console.error(error);
+    }
+
+});
+
+// get data - only online events
+
+
 
 module.exports = router;
