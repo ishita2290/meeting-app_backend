54ac7859021d8d8cc084a844596764f121f0c2e5
Marzena Studniarz <marzena.kupiec@gmail.com>
2021-01-26 09:41:51 +0100

    [event schema and event endpoints updated]

--- Models/schemas/EventSchema.js
+++ Models/schemas/EventSchema.js
@@ -2,22 +2,30 @@ const mongoose = require('mongoose');
 const User = require('../UserModel');
 
 const EventSchema = new mongoose.Schema({
-    //organizer: { type: mongoose.Schema.Types.ObjectId, ref: User, required: true }, // dynamic, must correspond with logged-in user
+    organizer: { type: mongoose.Schema.Types.ObjectId, ref: User, required: false }, // dynamic, must correspond with logged-in user
     eventName: { type: String, required: true, unique: true },
     startingDate: { type: Date, required: true },
-    finishingDate: { type: Date },
-    online: { type: Boolean },
-    location: { // GeoJSON https://mongoosejs.com/docs/geojson.html
-        type: {
-            type: String, 
-            enum: ['Point'] // 'location.type' must be 'Point'
-        },
-        coordinates: {
-            type: [Number],
-            required: false
-        }
+    timeFrom: { type: String, required: true },
+    timeTo: { type: String, required: true },
+    online: { type: Boolean, required: true },
+    // location: { // GeoJSON https://mongoosejs.com/docs/geojson.html
+    //     type: {
+    //         type: String, 
+    //         enum: ['Point'] // 'location.type' must be 'Point'
+    //     },
+    //     coordinates: {
+    //         type: [Number],
+    //         required: false
+    //     }
+    // },
+    address: {
+        place: { type: String },
+        street: { type: String },
+        postalCode: { type: Number },
+        city: { type: String },
+        country: { type: String },
     },
-    description: { type: String },
+    description: { type: String, unique: true },
     category: { type: String, enum: ['music', 'books', 'sport', 'learning languages', 'other'] },
     //participants: { type: [mongoose.Schema.Types.ObjectId] } // dynamic 
 });
