aed7991dcbaf6d2b56018161536dcbd78f3db4fe
ishita <ishitag077@gmail.com>
2021-02-07 19:40:10 +0100

    backend updated

--- routes/events.js
+++ routes/events.js
@@ -1,136 +1,136 @@
-const router = require('express').Router();
-const { builtinModules } = require('module');
-const mongoose = require('mongoose');
-const Event = require('../Models/EventModel');
+const router = require("express").Router();
+const { builtinModules } = require("module");
+const mongoose = require("mongoose");
+const Event = require("../Models/EventModel");
 
-router.post('/add-new-event', async (request, response) => {
-
-    try {
-        const createNewEvent = await Event.create(request.body);
-        //console.log('it worked');
-        response.status(201).json(
-            {
-                status: 'New Event added',
-                data: createNewEvent                                     
-            }
-        )
-    }
-    catch (error) {
-        console.error(error);
-    }
-       
+router.post("/add-new-event", async (request, response) => {
+  try {
+    const createNewEvent = await Event.create(request.body);
+    //console.log('it worked');
+    response.status(201).json({
+      status: "New Event added",
+      data: createNewEvent,
+    });
+  } catch (error) {
+    console.error(error);
+  }
 });
 
-<<<<<<< HEAD
-router.get("/get-event/:id" , async (request, response) => {
-    const singleEvent = await Event.findById(request.params.id);
-    if(!singleEvent){
-       return response.send('not correct id')
-    }
-    response.send(singleEvent)
-})
-
-=======
->>>>>>> b43caff57f7f39f9ad2a27571c1d743aef00d139
-// get data organized by provided user
-router.get('/get-organized-events', async (request, response) => {
-
-    try {
-        if (typeof request.query.user !== 'undefined' && request.query.user !== '') {
-            var user = JSON.parse(request.query.user);
-
-            var ObjectId = mongoose.Types.ObjectId;
-
-            const events = await Event.find({ organizer: new ObjectId(user._id)});
-
-            if (events.length > 0) {
-                response.json({status: true, events: events});
-            } else {
-                response.json({status: false});
-            }
-        } else {
-            response.json({status: false});
-        }
-    } catch (error) {
-        console.error(error);
-        response.json({status: false});
-    }
-
+router.get("/get-event/:id", async (request, response) => {
+  const singleEvent = await Event.findById(request.params.id);
+  if (!singleEvent) {
+    return response.send("not correct id");
+  }
+  response.send(singleEvent);
 });
 
 // get data organized by provided user
-router.get('/get-attended-events', async (request, response) => {
+router.get("/get-organized-events", async (request, response) => {
+  try {
+    if (
+      typeof request.query.user !== "undefined" &&
+      request.query.user !== ""
+    ) {
+      var user = JSON.parse(request.query.user);
 
-   try {
-        if (typeof request.query.user !== 'undefined' && request.query.user !== '') {
-            var user = JSON.parse(request.query.user);
+      var ObjectId = mongoose.Types.ObjectId;
 
-            var ObjectId = mongoose.Types.ObjectId;
+      const events = await Event.find({ organizer: new ObjectId(user._id) });
 
-            const eventAttended = await Event.find({ participants: new ObjectId(user._id) });
-
-            if (eventAttended.length > 0) {
-                response.json({status: true, events: eventAttended});
-            } else {
-                response.json({status: false});
-            }
-        } else {
-            response.json({status: false});
-        }
-    } catch (error) {
-        console.error(error);
-        response.json({status: false});
+      if (events.length > 0) {
+        response.json({ status: true, events: events });
+      } else {
+        response.json({ status: false });
+      }
+    } else {
+      response.json({ status: false });
     }
+  } catch (error) {
+    console.error(error);
+    response.json({ status: false });
+  }
+});
 
+// get data organized by provided user
+router.get("/get-attended-events", async (request, response) => {
+  try {
+    if (
+      typeof request.query.user !== "undefined" &&
+      request.query.user !== ""
+    ) {
+      var user = JSON.parse(request.query.user);
+
+      var ObjectId = mongoose.Types.ObjectId;
+
+      const eventAttended = await Event.find({
+        participants: new ObjectId(user._id),
+      });
+
+      if (eventAttended.length > 0) {
+        response.json({ status: true, events: eventAttended });
+      } else {
+        response.json({ status: false });
+      }
+    } else {
+      response.json({ status: false });
+    }
+  } catch (error) {
+    console.error(error);
+    response.json({ status: false });
+  }
 });
 
 // get data based on event's name
 
-router.get('/search-events/name/:query', async (request, response) => {
+router.get("/search-events/name/:query/:numOfResults/:skipResults", async (request, response) => {
+  const { query } = request.params;
+  const numOfResults = request.params.numOfResults || 10;
+    const skipResults = request.params.skipResults || 0;
 
-    const {query} = request.params;
-
-    try {
-        const nameWhichContains = query;
-        const regex = new RegExp(nameWhichContains, 'i');
-        const events = await Event.find({eventName: {$regex: regex}});
-        response.json(events);
-    }
-    catch (error) {
-        console.error(error);
-    }
-
+  try {
+    const nameWhichContains = query;
+    const regex = new RegExp(nameWhichContains, "i");
+    const events = await Event.find({ eventName: { $regex: regex } }).limit(Number(numOfResults)).skip(Number(skipResults));
+    response.json(events);
+  } catch (error) {
+    console.error(error);
+  }
 });
 
 // get data based on category
 
-router.get('/search-events/category/:category', async (request, response) => {
-
+router.get(
+  "/search-events/category/:category/:numOfResults/:skipResults",
+  //
+  async (request, response) => {
     const category = request.params.category;
+    const numOfResults = request.params.numOfResults || 10;
+    const skipResults = request.params.skipResults || 0;
+    
+
+    //skip method
 
     try {
-        const events = await Event.find({category});
-        response.json(events);
-    }
-    catch (error) {
-        console.error(error);
-    }
+      const events = await Event.find({ category }).limit(Number(numOfResults)).skip(Number(skipResults));
+     
 
-});
+      response.json(events);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+);
 
 // get data - only online events
 
-router.get('/search-events/online', async (request, response) => {
-
-    try {
-        const events = await Event.find({online: true});
-        response.json(events);
-    }
-    catch (error) {
-        console.error(error);
-        response.status(404).send('/here was an error')
-    }
-
+router.get("/search-events/online", async (request, response) => {
+  try {
+    const events = await Event.find({ online: true });
+    response.json(events);
+  } catch (error) {
+    console.error(error);
+    response.status(404).send("/here was an error");
+  }
 });
 
 //get data based on city
--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -4,8 +4,8 @@ const sendEmail = async (options) => {
     
     let transporter = nodemailer.createTransport({
         host : process.env.SMTP_HOST,
-        // port : process.env.SMTP_PORT,
-        // secure : false, // true 465 , false for other ports
+        port : process.env.SMTP_PORT,
+        secure : false, // true 465 , false for other ports
      
         auth : {
             user:process.env.SMTP_USER,
@@ -23,9 +23,16 @@ const sendEmail = async (options) => {
     text: options.text, // plain text body
     html: options.html, // html body
   }
-    let info = await transporter.sendEmail(message);
+    
+     try {
+      let info = await transporter.sendMail(message);
+        console.log('info  : ',info);
+     } catch (error) {
+       console.log('sendEmail error : ',error);
+     }
   
-    console.log("Message sent: %s", info.messageId);    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+  
+    // console.log("Message sent: %s", info.messageId);    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
   
    
   }
